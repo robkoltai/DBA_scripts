8a914aaa19cce60c723c399c069b23f47d73e64e
robkoltai <robkoltai@users.noreply.github.com>
2017-09-01 10:46:25 +0200

    more params

--- DG/dg_param.sql
+++ DG/dg_param.sql
@@ -1,13 +1,11 @@
 set lines 150
-column name format a25
-column value format a120
+column name format a20
+column value format a70
 select name, value from v$parameter where name in ( 
         'log_archive_dest_1',
         'log_archive_dest_2',
         'log_archive_config',
-        'fal_server',
-		'db_unique_name',
-		'standby_file_management'
+        'fal_server'
 )
 order by name;
 
